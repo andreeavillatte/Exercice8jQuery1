34bf7d85b9b47efd0aaf06ba8b553f76a9b7a95f
core.editor <andreeavillatte@gmail.com>
2019-04-23 12:28:05 +0200

    après correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     //si je clique sur le bouton
     $('#button').click(function () {
     // je donne la couleur red a tous les éléments li de ol
-    $('ol li').css('color','red');
+    $('ol>li').css('color','red');
     });
 });
